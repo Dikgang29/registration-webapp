5da80188e32c269e86cd57b9c7b4712b9a7643b1
Dikgang29 <dcmokonyama29@gmail.com>
2022-10-25 19:33:01 +0200

    making it better tying to fix logs

--- index.js
+++ index.js
@@ -70,20 +70,22 @@ app.post('/reg_number', async (req,res)=>{
 
     } else if(regInput){
         // testing 
-        const testing1 = await regBD.checkingDuplictes(regInput);
-        if(testing1 != 0){
-            req.flash('error', 'Registration already exists');
-            // res.redirect('/')
+        // const testing1 = await regBD.checkingDuplictes(regInput);
+        // if(testing1 != 0){
+        //     req.flash('error', 'Registration already exists');
+        //     // res.redirect('/')
             
-        } else if(testing1 == 0){
-            req.flash('success', 'Registration added succefully');
-            await regBD.insertReg(townRegNumber);
-            // res.redirect('/')
+        // } else if(testing1 == 0){
+        //     req.flash('success', 'Registration added succefully');
+        //     await regBD.insertReg(townRegNumber);
+        //     // res.redirect('/')
 
-        }
-        //  await regBD.insertReg(townRegNumber);
+        // }
+         await regBD.insertReg(townRegNumber);
         res.redirect('/')
     } 
+    res.redirect('/')
+
 });
 
 // radio buttom post form 
