dfedef0f8bd55ee2a2e5cc92197d81de3e676880
Dikgang29 <dcmokonyama29@gmail.com>
2022-10-25 15:03:49 +0200

    making it better tying to fix logs

--- index.js
+++ index.js
@@ -68,10 +68,11 @@ app.post('/reg_number', async (req,res)=>{
     if(!regInput){
         req.flash('error', 'Please enter the town registration and then select the ADD button');
     } else if(regInput){
+        // testing 
         const testing1 = await regBD.checkingDuplictes(regInput);
         if(testing1 != 0){
             req.flash('error', 'Registration already exists');
-        } else{
+        } else if(testing1 == 0){
             req.flash('success', 'Registration added succefully');
             await regBD.insertReg(townRegNumber);
 
