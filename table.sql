8d4f77445808a0e08dcc2260e2bd1d59fd13f08b
Dikgang Mokonyama <dcmokonyama29@gmail.com>
2022-11-04 11:29:47 +0200

    making it better

--- table.sql
+++ table.sql
@@ -17,3 +17,17 @@ CREATE TABLE regTest(
     id serial primary key,
     reg text not null
 );
+
+create database reg_app;
+create role reg_admin login password 'registration';
+grant all privileges on database reg_app to reg_admin;
+
+-- insert values in tables
+
+insert into reg_cities (reg_code,town)
+values  ('NB','Bergville'),
+    ('ND','Durban'),
+    ('NA','Harding'),
+    ('NN','Newcastle');
+
+
